10d97be06d8c54495d87a4ce1b6390fbe404904e
Rashi Ratan <rashiiratan@gmail.com>
2024-08-30 17:52:21 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+//Task 1: Variables and Data Types
+
+let employeeName = "John Doe";
+const employeeID = 1;
+var isActive = false;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
