44ac70153d9d6501ac9335ea254c93f9ce6e9a0b
Rashi Ratan <rashiiratan@gmail.com>
2024-08-30 19:13:02 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,16 @@ var isActive = false;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+
+
+//Task 2: Compound Data Types
+
+let products = ["apple", "banana", "grape"]
+const productDetails = {
+        name: products,
+        price: 50.00,
+        inStock: "yes",
+    }
+
+console.log(products, productDetails);
